e8ca2e04ca0070c7e83d1d39bc6826f7b9011232
Sam Mingolelli <github@jake8us.org>
2018-10-06 17:24:20 -0400

    Improves box building script to trim unnecessary bits & include a more basic Vagrantfile in .box

--- centos_7_1node_openshift/make_box.bash
+++ centos_7_1node_openshift/make_box.bash
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-vagrant package --vagrantfile Vagrantfile --output centos_7_1node_openshift_$(<VERSION).box
+vagrant ssh <<-EOF
+set -x
+sudo rm -f /swapfile
+sudo dd if=/dev/zero of=/EMPTY bs=1M &> /dev/null
+sudo rm -f /EMPTY
+set +x
+EOF
+
+#sudo oc adm prune deployments --confirm
+#sudo oc adm prune builds --confirm
+#sudo oc adm prune images --confirm > /dev/null 2>&1 
+
+vagrant package --vagrantfile Vagrantfile.prod --output ~/centos_7_1node_openshift_$(<VERSION).box
 
 echo ""
 echo "************"
